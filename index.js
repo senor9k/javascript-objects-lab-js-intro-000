2f409f64ca4096fbe0956c0e1a4ea382b197e44b
legacy-collaborator-7770 <johnvasiliadis@me.com>
2019-01-05 03:19:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
   newObj[key] = value
   return newObj
 }
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
