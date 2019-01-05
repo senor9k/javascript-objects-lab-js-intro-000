8f1cdc41399f81952cbeb22b34d5ae6c26e860ba
legacy-collaborator-7770 <johnvasiliadis@me.com>
2019-01-05 03:07:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   return object
 }
+function updateObjectWithKeyAndValue(object, key, value) {
+  newObj = object.assign({},object)
+  newObj[key] = value
+  return newObj
+}
