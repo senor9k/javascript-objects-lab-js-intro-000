0e52c75806a032feccc5f4eed603ac2deb28f61f
legacy-collaborator-7770 <johnvasiliadis@me.com>
2019-01-05 03:03:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let recipes = {
   prop: 1
 }
 function updateObjectWithKeyAndValue(object, key, value) {
-  object.key = value
+  object[key] = value
   return object
 }
 updateObjectWithKeyAndValue(recipes, "prop2", 2)
